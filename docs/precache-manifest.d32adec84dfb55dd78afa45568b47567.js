fc3ae1109ed0954c1f6d16e80406187c0cd0cba2
ruinername <russhianhere@gmail.com>
2019-06-22 21:50:08 +0300

    gitignore fix and deploy

---
+++ docs/precache-manifest.d32adec84dfb55dd78afa45568b47567.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "993c0ec075171b72e9a022385a13adaf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ac38f3d2a0d44298574b",
+    "url": "/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "40286558a36945a68ade",
+    "url": "/static/js/2.c29c6a1b.chunk.js"
+  },
+  {
+    "revision": "ac38f3d2a0d44298574b",
+    "url": "/static/js/main.8480b6d8.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
