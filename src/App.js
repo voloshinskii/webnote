79546bbac9f6a1a9ca496febf90cb80d2da0a3a6
ruinername <russhianhere@gmail.com>
2019-06-22 21:39:19 +0300

    todo

--- src/App.js
+++ src/App.js
@@ -1,26 +1,37 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Editor from '@todokek/react-editor.js'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props){
+      super(props);
+      this.state = {
+        data: {}
+      }
+  }
+
+  componentWillMount(){
+    var data = localStorage.getItem('document1');
+    data = JSON.parse(data);
+    this.setState({data: data})
+  }
+
+  componentDidUpdate(){
+    localStorage.setItem('document1', JSON.stringify(this.state.data));
+
+  }
+
+  render(){
+    return (
+        <Editor
+                autofocus
+                holderId="editorjs-container"
+                onChange={(data) => this.setState({data: data})}
+                data={this.state.data}
+              />
+    );
+  }
 }
 
 export default App;
